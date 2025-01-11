af799a3aebcde8fef184d8d133c4043c80479140
DUDE <nisbis194@gmail.com>
2025-01-11 23:54:13 +0545

    stored all the dat abut it needs double click to get the data

--- music/src/searchHandle.tsx
+++ music/src/searchHandle.tsx
@@ -1,17 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Passcontext } from "./App";
+import { useState } from "react";
 
 export default function SearchHandle() {
   const YOUR_API_KEY = "AIzaSyDpYcH-GOMEBAJhe5zkRTLpQgZY4fmO_CI";
   const context = useContext(Passcontext);
+  // replicated the api data structure for easyness
+  interface YoutubeDataItem {
+    contentDetails: {
+      videoId: string;
+    };
+    snippet: {
+      position: number;
+      title: string;
+      thumbnails: {
+        maxres: {
+          url: URL;
+        };
+      };
+    };
+  }
 
   if (!context) {
     return <div>Error: Context is not available!</div>;
   }
 
   const { playid } = context;
+  const [handledata, setHandleData] = useState<YoutubeDataItem[]>([]);
+  const [handleId, setHandleId] = useState<string[]>([]);
+  const [handleTitle, setHandleTitle] = useState<string[]>([]);
+  const [handleThumbnail, setHandleThumbnail] = useState<URL[]>([]);
 
   const fetchYoutubeData = async () => {
+    let Apidata = null;
     try {
       const response = await fetch(
         `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails&maxResults=50&playlistId=${playid}&key=${YOUR_API_KEY}`
@@ -19,19 +40,46 @@ export default function SearchHandle() {
       if (!response.ok) {
         throw new Error("Failed To Fetch Data");
       }
-      const data = await response.json();
-      console.log(data);
-      console.log(data.items[0].snippet.title);
+      Apidata = await response.json();
+      setHandleData(Apidata.items); // Set the fetched data here
     } catch (Error) {
       console.error("error fetching youtube Data");
     }
   };
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(handledata);
+    }, 7000);
+  }, [handledata]);
+
+  // mapping over item of json and setting value of id title and thumbnail :)
+  // datas are parameter we are passing handle data through datas
+  function setData() {
+    if (handledata.length > 0) {
+      handledata.forEach((datas, index) => {
+        setHandleTitle((prev) => [...prev, datas.snippet.title]);
+        // setHandleThumbnail((prev) => [
+        //   ...prev,
+        //   datas.snippet.thumbnails.maxres.url,
+        // ]);
+      });
+    }
+    console.log(handleTitle); // This will show the updated titles
+  }
+
+  // useEffect(() => {
+  //   if (handledata.length > 0) {
+  //     setData(); // Call setData only when handledata is populated
+  //   }
+  // }, [handledata]); // Re-run the effect when handledata changes
 
   return (
     <div>
       <button onClick={fetchYoutubeData} className="border-2 border-black ">
         Search
       </button>
+      <h1>Hello dai</h1>
     </div>
   );
 }
